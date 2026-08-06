737793f9a786c10f6482cebc185f3f6875f0df24
pradeeptaware6269 <pradeeptaware6269@gmail.com>
2026-08-06 17:50:06 +0530

    6/8/2026

--- Pradeep.logic.VIIT.com/src/Day36/Task4.java
+++ Pradeep.logic.VIIT.com/src/Day36/Task4.java
@@ -1,35 +1,68 @@
 package Day36;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Task4 {
+
 	public List<List<Integer>> threeSum(int[] nums) {
-		List<Integer> lst = new ArrayList<Integer>();
 
-		List<List<Integer>> lst1 = new ArrayList<List<Integer>>();
+		Arrays.sort(nums);
+
+		List<List<Integer>> ans = new ArrayList<>();
+
 		int n = nums.length;
-		for (int i = 0; i < n; i++) {
-			for (int j = i + 1; j < n; j++) {
-				for (int k = j + 1; k < n; k++) {
-					int sum = nums[i] + nums[j] + nums[k];
-					if (sum == 0) {
-						lst.add(nums[i]);
-						lst.add(nums[j]);
-						lst.add(nums[k]);
 
-						lst1.add(lst);
+		for (int i = 0; i < n - 2; i++) {
+
+			if (i > 0 && nums[i] == nums[i - 1]) {
+				continue;
+			}
+
+			int left = i + 1;
+			int right = n - 1;
+
+			while (left < right) {
+
+				int sum = nums[i] + nums[left] + nums[right];
+
+				if (sum == 0) {
+
+					ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+					left++;
+					right--;
+
+					while (left < right && nums[left] == nums[left - 1]) {
+						left++;
 					}
+
+					while (left < right && nums[right] == nums[right + 1]) {
+						right--;
+					}
+
+				} else if (sum < 0) {
+
+					left++;
+
+				} else {
+
+					right--;
+
 				}
 			}
 		}
-		return lst1;
+
+		return ans;
 	}
 
 	public static void main(String[] args) {
-		int arr[] = { -1, 0, 1, 2, -1, -4 };
+
+		int arr[] = { -100, -70, -60, 110, 120, 130, 160 };
+
 		Task4 t = new Task4();
 		System.out.println(t.threeSum(arr));
-	}
 
+	}
 }
