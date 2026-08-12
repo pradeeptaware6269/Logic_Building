a5b70963183367891ca26f8fac2ac9a79652b4f7
pradeeptaware6269 <pradeeptaware6269@gmail.com>
2026-08-12 19:12:44 +0530

    12/8/2026

--- Pradeep.logic.VIIT.com/src/Day39/Task3.java
+++ Pradeep.logic.VIIT.com/src/Day39/Task3.java
@@ -1,5 +1,46 @@
 package Day39;
 
+import java.util.Arrays;
+import java.util.Vector;
+
 public class Task3 {
+	public int[] rearrangeArray(int[] nums) {
+
+		int n = nums.length;
+
+		Vector<Integer> pos = new Vector<Integer>();
+		Vector<Integer> neg = new Vector<Integer>();
+
+		for (int i = 0; i < n; i++) {
+
+			if (nums[i] > 0) {
+				pos.add(nums[i]);
+			} else {
+				neg.add(nums[i]);
+			}
+		}
+
+		int p = 0;
+		int ng = 0;
+
+		for (int i = 0; i < n; i++) {
+
+			if (i % 2 == 0) {
+				nums[i] = pos.get(p++);
+			} else {
+				nums[i] = neg.get(ng++);
+			}
+		}
+
+		return nums;
+
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 3, 1, -2, -5, 2, -4 };
+		Task3 t = new Task3();
+		System.out.println(Arrays.toString(t.rearrangeArray(arr)));
+
+	}
 
 }
---
+++ Pradeep.logic.VIIT.com/src/Day39/Task4.java
@@ -0,0 +1,13 @@
+package Day39;
+
+public class Task4 {
+	public boolean validMountainArray(int[] arr) {
+
+		return false;
+	}
+	public static void main(String[] args) {
+		int arr[] = { 0, 3, 2, 1 };
+		Task4 t = new Task4();
+		System.out.println(t.validMountainArray(arr));
+	}
+}
